1bbe78c968c1578ad9007491e14ec9fb77d0d97a
Srushti Madaiah Basavaraju <hvh9084@autuni.ac.nz>
2023-10-10 16:14:18 +1300

    adding login page

---
+++ frontend/src/pages/login/sign_up.tsx
@@ -0,0 +1,13 @@
+
+
+export default function SignUp() {
+    return (
+      <div className="container">
+        <h1>Sign UP Page </h1>
+        <p> Please fill The sign up page 
+        </p>
+        
+      </div>
+    );
+  }
+  
