9c8e2650ff02e6b023de41eca6d32df2f4015553
HazeemHussain <hhzmuk@gmail.com>
2023-10-22 20:37:35 +1300

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+// app.js
+
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
@@ -10,29 +12,16 @@ const app = express();
 // Connect Database
 connectDB();
 
-// CORS Configuration
-app.use(cors({
-  origin: 'https://assignment-1-b-amber.vercel.app/', // Replace with your Vercel frontend URL
-  credentials: true
-}));
-
-// Middleware for logging each request
-app.use((req, res, next) => {
-  console.log(`Received ${req.method} request for ${req.url}`);
-  next();
-});
+// cors
+app.use(cors());
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Use Routes
-app.use('/api/article', articles);
+//app.get('/', (req, res) => res.send('Hello world!'));
 
-// General error handler
-app.use((error, req, res, next) => {
-  console.error(error.message);
-  res.status(500).send('Server Error');
-});
+// use Routes
+app.use('/api/article', articles);
 
 const port = process.env.PORT || 8082;
 
